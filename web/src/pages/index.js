d33e4ceed5febebf19117a63b0bdbdb4a138a285
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-04 21:07:13 +0200

    contain test on mobile screens + SEO change

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from '../components/seo';
 
 const IndexPage = () => (
   <>
-    <SEO title="Gala Groove Group" />
+    <SEO title="Gala Groove" />
     <HeroSection />
     <div className="container">
       <FeaturedBlogs />
