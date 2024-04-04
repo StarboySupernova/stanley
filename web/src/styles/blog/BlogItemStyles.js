7ccde222cdc325694eb5b055baa00289556dc19c
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-04 02:34:15 +0200

    #649a46

--- web/src/styles/blog/BlogItemStyles.js
+++ web/src/styles/blog/BlogItemStyles.js
@@ -4,7 +4,7 @@ export const BlogItemStyles = styled.div`
   display: inline-block;
   border-radius: 12px;
   padding: 10px;
-  background: #1f1e03;
+  background: #649a46;
   border: 0.5px solid rgba(255, 255, 255, 0.5);
   box-shadow: 0px 50px 100px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(20px);
