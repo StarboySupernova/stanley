086ebbfa43588622e79dcc812c2eb58e95d93afb
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-04 02:04:48 +0200

    #040205

--- web/src/styles/GlobalStyles.js
+++ web/src/styles/GlobalStyles.js
@@ -11,7 +11,7 @@ const GlobalStyles = createGlobalStyle`
     --secondary: #02634b;
     --darkBlue: #070747; 
     --darkPurple: #0E0034;
-    --black-1: #0D121F;
+    --black-1: #040205;
     --black-2: #121826;
     --white-1: #C6BED9;
     --gray: #64718A;
