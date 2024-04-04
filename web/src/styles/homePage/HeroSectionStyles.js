2ebdb175a560d44ca2469ef41633df3aec4f3f02
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-04 03:24:50 +0200

    herosectionstyles

--- web/src/styles/homePage/HeroSectionStyles.js
+++ web/src/styles/homePage/HeroSectionStyles.js
@@ -19,7 +19,7 @@ export const HeroSectionStyles = styled.div`
       left: 0;
       width: 50%;
       height: 100%;
-      background: linear-gradient(135deg, #0a0112, #08011c);
+      background: linear-gradient(135deg, #000000, #130f40);
       z-index: -1;
     }
     .left {
