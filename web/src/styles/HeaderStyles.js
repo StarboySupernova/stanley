af85991fd61d3ffcc51727c846b8e2ff9ca199a4
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-06 10:20:44 +0200

    header color - --black-1

--- web/src/styles/HeaderStyles.js
+++ web/src/styles/HeaderStyles.js
@@ -28,7 +28,7 @@ export default styled.header`
           }
         }
         a {
-          color: var(--primary);
+          color: var(--black-1);
           display: inline-block;
           text-decoration: none;
           font-size: 1.6rem;
