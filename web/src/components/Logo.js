7247cdecc974be035790ce6ba4a158b28e471ceb
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-05-07 04:15:02 +0200

    new logo

--- web/src/components/Logo.js
+++ web/src/components/Logo.js
@@ -8,7 +8,11 @@ function Logo() {
   const height = 50;
   return (
     <LogoStyles to="/">
-      <StaticImage src="../images/mwenje.jpg" width={width} height={height} />
+      <StaticImage
+        src="../images/Papirus-Team-Papirus-Apps-Scummvm.512.png"
+        width={width}
+        height={height}
+      />
     </LogoStyles>
   );
 }
